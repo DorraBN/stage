fd176b1895934d84f28e7e04392d1869fe582fee
DorraBN <dorransira18@gmail.com>
2024-07-26 16:35:36 +0100

    updates

--- frontend/restaurent/lib/screens/dashboard/pages/verification.dart
+++ frontend/restaurent/lib/screens/dashboard/pages/verification.dart
@@ -1,51 +1,83 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert'; // Pour utiliser jsonDecode
+import 'dart:convert'; // To use jsonDecode
 
 import 'package:restaurent/core/constants/color_constants.dart';
-import 'package:restaurent/core/utils/colorful_tag.dart';
 import 'package:colorize_text_avatar/colorize_text_avatar.dart';
 
-// API URL (remplacez par l'URL de votre API)
-const String apiUrl = 'http://localhost:8000/api/reservations?email=dorra@gmail.com'; 
+// API URL (replace with your API URL)
+const String baseUrl = 'http://localhost:8000/api/reservations';
 
 Future<List<UserReservation>> fetchUserDataByEmail(String email) async {
-  final response = await http.get(Uri.parse('$apiUrl?email=$email'));
+  final Uri apiUrl = Uri.parse(baseUrl).replace(queryParameters: {'email': email});
+
+  print('Fetching data from: $apiUrl'); // Debug print
+  print('Email: $email'); // Debug print
+
+  try {
+    final response = await http.get(apiUrl);
+
+    print('Response status: ${response.statusCode}'); // Debug print
+    print('Response body: ${response.body}'); // Debug print
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      return data.map((json) => UserReservation.fromJson(json)).toList();
+    } else {
+      print('Failed to load reservations');
+      return [];
+    }
+  } catch (e) {
+    print('Error: $e');
+    return [];
+  }
+}
+
+Future<void> deleteReservation(String id) async {
+  final response = await http.delete(Uri.parse('http://127.0.0.1:8000/api/deletereserves/$id'));
 
   if (response.statusCode == 200) {
-    final List<dynamic> data = jsonDecode(response.body);
-    return data.map((json) => UserReservation.fromJson(json)).toList();
+    // Successfully deleted
   } else {
-    throw Exception('Failed to load reservations');
+    throw Exception('Failed to delete reservation');
   }
 }
 
 class UserReservation {
+  final int id;
   final String name;
   final String phoneNumber;
-  final int numberOfPeople;
+  final String email;
+  final String numberOfPeople;
   final String reservationDate;
   final String reservationTime;
+   final String message;
   final String status;
 
   UserReservation({
+    required this.id,
     required this.name,
     required this.phoneNumber,
+    required this.email,
     required this.numberOfPeople,
     required this.reservationDate,
     required this.reservationTime,
-    required this.status,
+      required this.message,
+    required this.status, 
   });
 
   // Convert JSON to UserReservation
   factory UserReservation.fromJson(Map<String, dynamic> json) {
     return UserReservation(
-      name: json['name'],
-      phoneNumber: json['phoneNumber'],
-      numberOfPeople: json['numberOfPeople'],
-      reservationDate: json['reservationDate'],
-      reservationTime: json['reservationTime'],
-      status: json['status'],
+      id: json['id'],
+      name: json['name'] ?? 'Unknown',
+      phoneNumber: json['phone'] ?? 'Unknown',
+      email: json['email'] ?? 'Unknown',
+      numberOfPeople: json['person'] ?? '0',
+      reservationDate: json['reservation_date'] ?? 'Unknown',
+      reservationTime: json['time'] ?? 'Unknown',
+      message: json['message'] ?? 'Unknown',
+      status: 'Confirmed',// assuming status is always 'Confirmed' as it's not in JSON
     );
   }
 }
@@ -67,6 +99,47 @@ class _VerificationPageState extends State<VerificationPage> {
   void initState() {
     super.initState();
     _reservations = fetchUserDataByEmail(widget.email);
+  }
+
+  void _deleteReservation(int id) async {
+    await deleteReservation(id.toString());
+    setState(() {
+      _reservations = fetchUserDataByEmail(widget.email);
+    });
+  }
+
+  void _showReservationDetailsDialog(BuildContext context, UserReservation reservation) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Reservation Details'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('Name: ${reservation.name}'),
+                Text('Phone Number: ${reservation.phoneNumber}'),
+                Text('Email: ${reservation.email}'),
+                Text('Number of People: ${reservation.numberOfPeople}'),
+                Text('Reservation Date: ${reservation.reservationDate}'),
+                Text('Reservation Time: ${reservation.reservationTime}'),
+                                Text('Message: ${reservation.message}'),
+
+                Text('Status: ${reservation.status}'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -173,7 +246,7 @@ class _VerificationPageState extends State<VerificationPage> {
                 upperCase: true,
                 numberLetters: 1,
                 shape: Shape.Rectangle,
-                text: reservation.name[0],
+                text: reservation.name.isNotEmpty ? reservation.name[0] : 'N/A',
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
@@ -187,7 +260,7 @@ class _VerificationPageState extends State<VerificationPage> {
           ),
         ),
         DataCell(Text(reservation.phoneNumber)),
-        DataCell(Text(reservation.numberOfPeople.toString())),
+        DataCell(Text(reservation.numberOfPeople)),
         DataCell(Text(reservation.reservationDate)),
         DataCell(Text(reservation.reservationTime)),
         DataCell(Container(
@@ -206,7 +279,9 @@ class _VerificationPageState extends State<VerificationPage> {
             children: [
               IconButton(
                 icon: Icon(Icons.visibility, color: greenColor),
-                onPressed: () {},
+                onPressed: () {
+                  _showReservationDetailsDialog(context, reservation);
+                },
               ),
               SizedBox(width: 6),
               IconButton(
@@ -219,7 +294,57 @@ class _VerificationPageState extends State<VerificationPage> {
               IconButton(
                 icon: Icon(Icons.delete, color: Colors.redAccent),
                 onPressed: () {
-                  // Implement delete logic
+                  showDialog(
+                    context: context,
+                    builder: (_) {
+                      return AlertDialog(
+                        title: Center(
+                          child: Column(
+                            children: [
+                              Icon(Icons.warning_outlined, size: 36, color: Colors.red),
+                              SizedBox(height: 20),
+                              Text("Confirm Deletion"),
+                            ],
+                          ),
+                        ),
+                        content: Container(
+                          height: 70,
+                          child: Column(
+                            children: [
+                              Text(
+                                  "Are you sure you want to delete '${reservation.name}'?"),
+                              SizedBox(height: 16),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  ElevatedButton.icon(
+                                    icon: Icon(Icons.close, size: 14),
+                                    style: ElevatedButton.styleFrom(
+                                        backgroundColor: Colors.grey),
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    label: Text("Cancel"),
+                                  ),
+                                  SizedBox(width: 20),
+                                  ElevatedButton.icon(
+                                    icon: Icon(Icons.delete, size: 14),
+                                    style: ElevatedButton.styleFrom(
+                                        backgroundColor: Colors.red),
+                                    onPressed: () {
+                                      _deleteReservation(reservation.id);
+                                      Navigator.of(context).pop();
+                                    },
+                                    label: Text("Delete"),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
                 },
               ),
             ],
